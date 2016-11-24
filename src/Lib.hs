236711540374906863a3e2ce3608e03b035003fc
Alain Lehmann <alain.lehmann@gmail.com>
2016-11-24 22:38:06 +0100

    using basic characters only
    - no dot yet

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,12 +9,20 @@ import Text.Parsec
 data Email = Email String [String]
   deriving (Eq, Show)
 
+latin = ['a'..'z'] ++ ['A'..'Z']
+digits = ['0'..'9']
+specials = "!#$%&'*+-/=?^_`{|}~"
+baseChars = oneOf $ latin ++ digits ++ specials
+
 dnsLabel =  many1 $ noneOf "."
 domainParser = dnsLabel `sepBy1` char '.'
 
+localParser = many1 baseChars
+
+
 emailParser :: Parsec String st Email
 emailParser = do
-        localPart <- many1 $ noneOf "@"
+        localPart <- localParser
         char '@'
         domainPart <- domainParser
         return $ Email localPart domainPart
