810101fa4ff763d745939eaae1b5a18b7b4bc768
Alain Lehmann <alain.lehmann@gmail.com>
2016-11-24 23:09:05 +0100

    handling escaped chars

--- src/Lib.hs
+++ src/Lib.hs
@@ -17,11 +17,16 @@ baseChars = oneOf $ latin ++ digits ++ specials
 
 -- characters allowed within quotes
 quotedChars = oneOf " (),:;<>@[]."
-escapedChars = "\"\\"
+escapedChars = oneOf "\"\\"
+
+quotedString = char '"' *> stringInQuotes <* char '"'
+  where stringInQuotes = mconcat <$> many1 charsInQuotes
+        charsInQuotes = many1 (baseChars <|> quotedChars) <|> escapedChars2
+        escapedChars2 = (:) <$> char '\\' <*> count 1 escapedChars
+
 
 -- the local part
-localParserPart = char '"' *> many1 (baseChars <|> quotedChars) <* char '"'
-              <|> many1 baseChars
+localParserPart = quotedString <|> many1 baseChars
 localParser = mconcat <$> localParserPart `sepBy1` char '.'
 
 dnsLabel =  many1 $ noneOf "."
