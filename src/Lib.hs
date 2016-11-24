05e3ecdc34c09029bfca8d3ba81bb2be04179a74
Alain Lehmann <alain.lehmann@gmail.com>
2016-11-24 22:51:23 +0100

    support quoted characters & dots

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,15 +9,23 @@ import Text.Parsec
 data Email = Email String [String]
   deriving (Eq, Show)
 
+-- the very basic characters
 latin = ['a'..'z'] ++ ['A'..'Z']
 digits = ['0'..'9']
 specials = "!#$%&'*+-/=?^_`{|}~"
 baseChars = oneOf $ latin ++ digits ++ specials
 
+-- characters allowed within quotes
+quotedChars = oneOf " (),:;<>@[]."
+escapedChars = "\"\\"
+
+-- the local part
+localParserPart = char '"' *> many1 (baseChars <|> quotedChars) <* char '"'
+              <|> many1 baseChars
+localParser = mconcat <$> localParserPart `sepBy1` char '.'
+
 dnsLabel =  many1 $ noneOf "."
 domainParser = dnsLabel `sepBy1` char '.'
-
-localParser = many1 baseChars
 
 
 emailParser :: Parsec String st Email
