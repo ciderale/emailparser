a7c3705e0669e7e650126e84aa74664674b519e7
Alain Lehmann <alain.lehmann@gmail.com>
2016-11-25 13:57:58 +0100

    adding some type signatures

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RankNTypes #-}
 module Lib
     ( Email(..), Domain(..), IP(..)
     , emailParser
@@ -18,7 +19,12 @@ data IP = IPv4 Int Int Int Int
         | IPv6 String
         deriving (Eq, Show)
 
-emailParser :: Parsec String st Email
+-- a type alias for Parser with
+-- inputs of type 'String' and output of (generic) type 'a'
+type Parser a = forall st . Parsec String st a
+
+-- the main parser
+emailParser :: Parser Email
 emailParser = do
         localPart <- ignoreComment localParser
         char '@'
@@ -27,13 +33,15 @@ emailParser = do
         return $ Email localPart domainPart
 
 -- the local part & domain part
+localParser :: Parser String
 localParser = intercalate "." <$> localParserPart `sepBy1` dot
 localParserPart = (quotedString <|> manyOf1 baseChars) `limitedTo` 64
 
+domainParser :: Parser Domain
 domainParser = DomainIP <$> between2 "[]" ipParser
            <|> DomainName <$> dnsLabel `sepBy1` dot
 
-ipParser :: Parsec String st IP
+ipParser :: Parser IP
 ipParser = v6 <|> v4
     where v6 = string "IPv6:" >> IPv6 <$> many1 (noneOf "]")
           v4 = IPv4 <$> number <* dot <*> number <* dot <*> number <* dot <*> number
@@ -48,12 +56,13 @@ manyOf1 = many1 . oneOf
 ignoreComment = between (optional comment) (optional comment)
   where comment = between2 "()" (many1 (noneOf ")"))
 
+between2 :: String -> Parser a -> Parser a
+between2 [open,close] = between (char open) (char close)
+
+limitedTo :: Parser [a] -> Int -> Parser [a]
 p `limitedTo` len = do str <- p
                        guard $ length str <= len
                        return str
-
-between2 :: String -> Parsec String st a -> Parsec String st a
-between2 [open,close] = between (char open) (char close)
 
 -- the very basic characters
 dot = char '.'
@@ -63,9 +72,10 @@ specials = "!#$%&'*+-/=?^_`{|}~"
 baseChars = latin ++ digits ++ specials
 
 -- characters allowed within quotes
-quotedChars = " (),:;<>@[]."
-escapedChars = "\"\\"
+quotedChars = " (),:;<>@[]." -- allowed within double quotes
+escapedChars = "\"\\" -- double-quote and backslash must be escaped
 
+quotedString :: Parser String
 quotedString = do str <- char '"' *> stringInQuotes <* char '"'
                   return $ '"' : str ++ "\""
   where stringInQuotes = mconcat <$> many1 charsInQuotes
