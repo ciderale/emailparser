0bcddd844cadf123711b7be52c398bd144d32f5a
Alain Lehmann <alain.lehmann@gmail.com>
2016-11-27 21:57:08 +0100

    use either for case handling

--- app/ParserUtil.hs
+++ app/ParserUtil.hs
@@ -9,9 +9,12 @@ import Lib
 test :: Parser a -> (String -> a -> IO ()) -> String -> IO ()
 test parser onSuccess input = do
   putStrLn $ take 80 $ cycle "#"
-  let parseResult = parse parser input input
-  case parseResult of
-    Right value
-      -> onSuccess input value
-    Left error
-      -> putStrLn $ "failed to parse: " ++ show error
+  either (putStrLn . ("failed to parse: " ++) . show)
+         (onSuccess input)
+         (parse parser input input)
+  --let parseResult = parse parser input input
+  --case parseResult of
+  --  Right value
+  --    -> onSuccess input value
+  --  Left error
+  --    -> putStrLn $ "failed to parse: " ++ show error
